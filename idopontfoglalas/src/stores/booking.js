3f47578be28b6a01a4540ba1a2f2595b897eda7c
b0kovada <kovacshaziadam@gmail.com>
2025-03-05 14:45:21 +0100

    elindul

--- idopontfoglalas/src/stores/booking.js
+++ idopontfoglalas/src/stores/booking.js
@@ -2,7 +2,17 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 
 export const useBookingStore = defineStore('booking', {
-
-
-  return { count, doubleCount, increment }
+  state: () => ({
+    bookings: []
+  }),
+  actions: {
+    async getAvailableSlots(){
+      const {data: booked} = await axios.get('http://localhost:3000/bookings')
+      const allSlots = Array.from({lenght: 8}, (_,i) => `${8 + i}:00`)
+      return allSlots.filter(slot => !booked.some(b => b.time === slot))
+    },
+    async bookSlot(time, name, phone){
+      await axios.post('http://localhost:3000/bookings', {time, name, phone})
+    }
+  }
 })
