2f8b4be8211e2810c710db634377fc858c080164
b0kovada <kovacshaziadam@gmail.com>
2025-03-07 08:42:35 +0100

    napok listában

--- idopontfoglalas/src/views/BookingListView.vue
+++ idopontfoglalas/src/views/BookingListView.vue
@@ -1,25 +1,38 @@
 <template>
-    <div>
-      <h2>Elérhető időpontok</h2>
-      <ul class="list-group">
-        <li v-for="slot in availableSlots" :key="slot.id" class="list-group-item">
-          <router-link :to="`/foglalas/${slot.id}`" class="btn btn-primary">
-            {{ slot.date }} - {{ slot.time }}
-          </router-link>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref, onMounted } from 'vue';
-  import { useBookingStore } from '@/stores/booking';
-  
-  const store = useBookingStore();
-  const availableSlots = ref([]);
-  
-  onMounted(async () => {
-    await store.fetchBookings();
-    availableSlots.value = await store.getAvailableSlots();
-  });
-  </script>
+  <div>
+    <h2>Elérhető időpontok</h2>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Nap</th>
+          <th>Időpont</th>
+          <th>Foglalás</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="slot in availableSlots" :key="slot.id">
+          <td>{{ slot.date }}</td>
+          <td>{{ slot.time }}</td>
+          <td>
+            <router-link :to="`/foglalas/${slot.id}`" class="btn btn-primary">
+              Foglalás
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useBookingStore } from '@/stores/booking';
+
+const store = useBookingStore();
+const availableSlots = ref([]);
+
+onMounted(async () => {
+  await store.fetchBookings();
+  availableSlots.value = await store.getAvailableSlots();
+});
+</script>
