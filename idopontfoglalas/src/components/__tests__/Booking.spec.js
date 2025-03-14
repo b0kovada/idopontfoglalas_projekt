6ef234bcd8c15dd82c3a10748b8e9e0da42f32e8
b0kovada <kovacshaziadam@gmail.com>
2025-03-14 08:45:21 +0100

    plusz tesztek

--- idopontfoglalas/src/components/__tests__/Booking.spec.js
+++ idopontfoglalas/src/components/__tests__/Booking.spec.js
@@ -13,7 +13,7 @@ const router = createRouter({
 });
 
 describe('BookingListView.vue', () => {
-  it('fetches and displays available slots', async () => {
+  it('Idopontok megjelenitese', async () => {
     axios.get.mockResolvedValue({ data: [{ date: 'Hétfő', time: '10:00' }] });
     const wrapper = mount(BookingListView);
     await new Promise(resolve => setTimeout(resolve, 50));
@@ -22,8 +22,25 @@ describe('BookingListView.vue', () => {
   });
 });
 
+describe('BookingListView.vue', () => {
+  it('Helyesen megjelenik', () => {
+    const wrapper = mount(BookingListView);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
+
+describe('BookingListView.vue', () => {
+  it('Osszes elerheto idopont megjelenik', async () => {
+    axios.get.mockResolvedValue({ data: [] });
+    const wrapper = mount(BookingListView);
+    await new Promise(resolve => setTimeout(resolve, 50));
+    
+    expect(wrapper.text()).toContain('Elérhető időpontokNapIdőpontFoglalásHétfő8:00FoglalásHétfő9:00FoglalásHétfő10:00FoglalásHétfő11:00FoglalásHétfő12:00FoglalásHétfő13:00FoglalásHétfő14:00FoglalásHétfő15:00FoglalásHétfő16:00FoglalásKedd8:00FoglalásKedd9:00FoglalásKedd10:00FoglalásKedd11:00FoglalásKedd12:00FoglalásKedd13:00FoglalásKedd14:00FoglalásKedd15:00FoglalásKedd16:00FoglalásSzerda8:00FoglalásSzerda9:00FoglalásSzerda10:00FoglalásSzerda11:00FoglalásSzerda12:00FoglalásSzerda13:00FoglalásSzerda14:00FoglalásSzerda15:00FoglalásSzerda16:00FoglalásCsütörtök8:00FoglalásCsütörtök9:00FoglalásCsütörtök10:00FoglalásCsütörtök11:00FoglalásCsütörtök12:00FoglalásCsütörtök13:00FoglalásCsütörtök14:00FoglalásCsütörtök15:00FoglalásCsütörtök16:00FoglalásPéntek8:00FoglalásPéntek9:00FoglalásPéntek10:00FoglalásPéntek11:00FoglalásPéntek12:00FoglalásPéntek13:00FoglalásPéntek14:00FoglalásPéntek15:00FoglalásPéntek16:00Foglalás');
+  });
+});
+
 describe('BookingFormView.vue', () => {
-  it('renders correctly', () => {
+  it('Helyesen megjelenik', () => {
     const wrapper = mount(BookingFormView, {
       global:{plugins: [router]}
     });
